01566e29a1a9556b9636fe1dbcb66d89be01e2ee
John-Sekora <sekora.github@gmail.com>
2018-08-11 18:12:41 -0700

    Completed lesson07

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/John Sekora/Documents/UWPythonCert/Summer/lesson06/djangoblog/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
